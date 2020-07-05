037f950a526d42d50d42de18c622293b8121516e
王国庆 <176901048@qq.com>
2020-07-05 20:43:17 +0800

    解决冲突

--- zuulserver1/src/main/java/lw/springcloud/zuul/service/Fenzhi2Hello2Service.java
+++ zuulserver1/src/main/java/lw/springcloud/zuul/service/Fenzhi2Hello2Service.java
@@ -1,4 +1,13 @@
 package lw.springcloud.zuul.service;
 
 public class Fenzhi2Hello2Service {
+    public static void main(String[] args)
+    {
+
+    }
+    //分支2继续添加 cd3方法
+    public  void  cd3()
+    {
+
+    }
 }
