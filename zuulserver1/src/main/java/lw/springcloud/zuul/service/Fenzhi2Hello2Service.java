9c11d47dd21a6120b0e813645845174afc5d14de
王国庆 <176901048@qq.com>
2020-07-05 20:47:28 +0800

    解决冲突

--- zuulserver1/src/main/java/lw/springcloud/zuul/service/Fenzhi2Hello2Service.java
+++ zuulserver1/src/main/java/lw/springcloud/zuul/service/Fenzhi2Hello2Service.java
@@ -3,17 +3,12 @@ package lw.springcloud.zuul.service;
 public class Fenzhi2Hello2Service {
     public static void main(String[] args)
     {
-<<<<<<< .merge_file_a13224
+
         
     }
     //分支4 在分支2基础上添加的方法
     public void abc()
     {
-
-    }
-}
-=======
->>>>>>> .merge_file_a09988
 
     }
     //分支2继续添加 cd3方法
