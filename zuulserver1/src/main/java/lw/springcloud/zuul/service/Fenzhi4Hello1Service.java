69e76f41459818670d37a53fd67fa63ad633da43
王国庆 <176901048@qq.com>
2020-07-13 09:50:38 +0800

    暂存本地

--- zuulserver1/src/main/java/lw/springcloud/zuul/service/Fenzhi4Hello1Service.java
+++ zuulserver1/src/main/java/lw/springcloud/zuul/service/Fenzhi4Hello1Service.java
@@ -1,4 +1,9 @@
 package lw.springcloud.zuul.service;
 
 public class Fenzhi2Hello4Service {
+
+    public static void main(String[] args)
+    {
+        System.out.println("分支4添加");
+    }
 }
--- zuulserver1/src/main/java/lw/springcloud/zuul/service/Fenzhi4Hello2Service.java
+++ zuulserver1/src/main/java/lw/springcloud/zuul/service/Fenzhi4Hello2Service.java
@@ -4,6 +4,6 @@ public class Fenzhi2Hello4Service {
 
     public static void main(String[] args)
     {
-        System.out.println("分支4添加");
+        System.out.println("分支4添加 第二次修改");
     }
 }
